cf968456e5f47098f4114b8607653e656fcff719
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2021-03-10 17:19:16 -0800

    Update pricing.ts
    Fake Token Graph!  Ignore me!  .... >.>

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -11,10 +11,17 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0x71850b7e9ee3f13ab46d67167341e4bdc905eef9', // HONEY
+
   '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d', // WXDAI
+  '0x8138300DbF684251dfFF80f005EEf2114C5b582B', // PyrpullRayne Futures Exchange StableToken
+  '0xbea891F8283ec431123521d2d82b5C10f55Ca6b9', // xDIE Futures Exchange StableToken
+  '0x5f49887120c883917A7aC192997bd2E98033Af62', // swXDAI0 Synthetic Wrapper for wXDAI
+  '0x35570b69a74e61BF54b07A8D785D05b1cBf5Bb4B', // swBTC Synthetic Wrapper for wBTC
+  '0x3fDe0345a6Fb8a717044B1F11b972124412C1603', // swETH Single Ingot Asset Reserve Undivisable
+  '0x3b4C345A161b03e8498dC6770Ce8CC5C1ae74571', // swETH1 Asset Reserve Fiat 200% Value for Ingot
+  '0x90BD73F2066c87e6d4d918F8453845cF63c5Eb2E', // SyFi Synthetic YFI wrapper 30k Supply
+  '0x3E973D3e5c4d4342F1fDA8F2e42948C36275C888', // wSYNC Wrapper for SYNC tokens
   '0xddafbb505ad214d7b80b1f830fccc89b60fb7a83', // USDC on xDai
-  '0x4ecaba5870353805a9f068101a40e0f32ed605c6', // Tether on xDai
   '0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1' // Wrapped Ether on xDai
 ]
 
